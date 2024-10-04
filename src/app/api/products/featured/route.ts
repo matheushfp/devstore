4d74e2609c0c9d133b99027de5dfb7ad15218007
matheushfp <mathzhf@gmail.com>
2024-10-04 18:37:18 -0300

    add loading in home page

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,10 @@
+import { setTimeout } from 'node:timers/promises'
+
 import data from '../data.json'
 
 export async function GET() {
+  setTimeout(2000)
+
   const featuredProducts = data.products.filter((product) => product.featured)
 
   return Response.json(featuredProducts)
